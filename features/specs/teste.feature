f7cb1d6e52ad7faf858b8ca671062b62e3f12b14
LucasKoritar23 <lucas.gimenes23@gmail.com>
2021-05-06 13:08:06 -0300

    Melhorias nos testes

--- features/specs/teste.feature
+++ features/specs/teste.feature
@@ -13,8 +13,32 @@ Funcionalidade: Criar um novo usuário na API mockada
   Contexto:
     Dado que eu tenha acesso à API de criação de Usuário
 
-  @create_user_mock
+  @post_user_mock
   Cenário: Validar criação de um novo usuário na base
     Dado que possua o payload de criação de usuário
     Quando realizar o request de criação
+    Então deve apresentar o code 201
+
+  @get_user_mock
+  Cenário: Validar consulta de um novo usuário na base
+    Dado que possua o payload de criação de usuário
+    Quando realizar o request de consulta
+    Então deve apresentar o code 201
+
+  @patch_user_mock
+  Cenário: Validar edição parcial de um novo usuário na base
+    Dado que possua o payload de criação de usuário
+    Quando realizar o request de edição parcial
+    Então deve apresentar o code 201
+
+  @put_user_mock
+  Cenário: Validar edição completa de um novo usuário na base
+    Dado que possua o payload de criação de usuário
+    Quando realizar o request de edição completa
+    Então deve apresentar o code 201
+
+  @delete_user_mock
+  Cenário: Validar delação de um novo usuário na base
+    Dado que possua o payload de criação de usuário
+    Quando realizar o request de deleção
     Então deve apresentar o code 201
--- features/step_definitions/teste_step.rb
+++ features/step_definitions/teste_step.rb
@@ -8,12 +8,43 @@ Dado("que possua o payload de criação de usuário") do
         "idade": 24,
         "profissão": "QA Engineer"
     }
-    @headers = {}
+    @headers = {"teste": "true" }
 end
 
 Quando("realizar o request de criação") do
-   @request = QasDockGem::RequestsAPI.new.execute_post("123", "https://run.mocky.io/v3/21bd531c-ecc7-46c1-9831-de5fe8a0b2ac", @payload, @headers)
-   log_util(@request)
+    @request = QasDockGem::RequestsAPI.new.execute_post({"token": @token, "uri": "https://run.mocky.io/v3/21bd531c-ecc7-46c1-9831-de5fe8a0b2ac", "payload": @payload, "headers": @headers})
+    log_util(@request)
+end
+
+Quando("realizar o request de consulta") do
+    @request = QasDockGem::RequestsAPI.new.execute_get({"token": @token, "uri": "https://run.mocky.io/v3/21bd531c-ecc7-46c1-9831-de5fe8a0b2ac", "payload": nil, "headers": @headers})
+    log_util(@request)
+end
+
+Quando("realizar o request de edição parcial") do
+    @payload = {
+        "nome": "Lucas Gimenes Koritar Patch",
+        "idade": 24,
+        "profissão": "QA Engineer"
+    }
+    @request = QasDockGem::RequestsAPI.new.execute_patch({"token": @token, "uri": "https://run.mocky.io/v3/21bd531c-ecc7-46c1-9831-de5fe8a0b2ac", "payload": @payload, "headers": @headers})
+    log_util(@request)
+end
+
+Quando("realizar o request de edição completa") do
+    @payload = {
+        "nome": "Lucas teste Koritar",
+        "idade": 25,
+        "profissão": "QA Engineer Sr"
+    }
+    @request = QasDockGem::RequestsAPI.new.execute_put({"token": @token, "uri": "https://run.mocky.io/v3/21bd531c-ecc7-46c1-9831-de5fe8a0b2ac", "payload": @payload, "headers": @headers})
+    log_util(@request)
+end
+
+Quando("realizar o request de deleção") do
+    @headers = {"teste": "true" }
+    @request = QasDockGem::RequestsAPI.new.execute_delete({"token": @token, "uri": "https://run.mocky.io/v3/21bd531c-ecc7-46c1-9831-de5fe8a0b2ac", "payload": nil, "headers": @headers})
+    log_util(@request)
 end
 
 Então("deve apresentar o code {int}") do |status_code|
